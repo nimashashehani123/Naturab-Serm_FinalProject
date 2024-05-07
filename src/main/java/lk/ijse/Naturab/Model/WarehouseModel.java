50ea2c0292d7aff9f04679083a2bda52ef966e98
T.L.Nimasha Shehani <nimashashehani0715@gmail.com>
2024-05-07 23:26:35 +0530

    90% done

---
+++ src/main/java/lk/ijse/Naturab/Model/WarehouseModel.java
@@ -0,0 +1,14 @@
+package lk.ijse.Naturab.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class WarehouseModel {
+    private String WId;
+    private String Location;
+    private String Capacity;
+}
